213f80dca6587b3bc9d01596f6f613ff0b4413be
greenmilk <117968340+greenmilk1531@users.noreply.github.com>
2024-11-09 08:51:07 +0900

    upload main page

---
+++ web.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import time
+from PIL import Image
+from streamlit_modal import Modal
+
+공지 = 0 #공지를 0으로 설정
+
+
+
+@st.dialog("공지사항") # 타이틀
+def vote():
+    st.info(infos)
+    bu1,bu2 = st.columns([3,10])
+    with bu1:
+        st.link_button("보러가기", lastlink)
+    with bu2:
+        st.link_button("2주년 카운트 다운", "https://wjmax.kr/wjmax2")
+
+#공지를 읽지 않았을때만 보여줌
+
+    st.title("WJMAX")
+
+with b:
+    st.link_button("카페", "https://cafe.naver.com/moogimogi")
+with c:
+    st.link_button("다운로드", "https://cafe.naver.com/steamindiegame/12309318")
+
+with soop:
+    st.link_button("숲(SOOP)","https://bj.afreecatv.com/doob32")
+
+
+with e:
+
+with d:
+    st.link_button("패치내역", "https://namu.wiki/w/%EC%99%81%EC%A0%9C%EC%9D%B4%EB%A7%A5%EC%8A%A4/%EC%97%85%EB%8D%B0%EC%9D%B4%ED%8A%B8%20%EB%82%B4%EC%97%AD#toc")
+with ha:
+    st.link_button("수록곡 보기","https://namu.wiki/w/%EC%99%81%EC%A0%9C%EC%9D%B4%EB%A7%A5%EC%8A%A4/%EC%88%98%EB%A1%9D%EA%B3%A1")
+with se:
+    st.link_button("검색하기","검색")
+
+
+st.text("*- 왁맥 공식사이트가 아닙니다")
+st.text("원작게임: DJMAX")
